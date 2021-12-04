03c65ae3cb4e6408f8f58cda983b4ca1960cf95c
Simon Pope <sdpope@gmail.com>
2021-12-04 17:29:30 -0330

    nothing much yet

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Sandbox from "./sandbox";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        Hello
+      </div>
+      <Switch>
+        <Route path="/" exact>
+        <p> Main page</p>
+        </Route>
+        <Route path="/about" exact>
+          <p>About page</p>
+        </Route>
+        <Route path="/sandbox" exact> 
+          <Sandbox />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+
+    
   );
 }
 
